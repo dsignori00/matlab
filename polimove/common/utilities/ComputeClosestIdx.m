638fe7750dceb5020740199dc6e6df85422a365e
Daniele Signori <daniele.signori00@gmail.com>
2025-06-25 17:44:46 +0200

    [Common] new compute idx func vectorized

--- polimove/common/utilities/ComputeClosestIdx.m
+++ polimove/common/utilities/ComputeClosestIdx.m
@@ -1,22 +1,17 @@
-function [closestIdx] = compute_closestIdx(x, y, traj_struct)
-% compute_closestIdx: given the (x,y) position of a vehicle inside the track
-% and a trajecotry computes the closest point of the trajectory to the
-% vehicle
-%% compute_closestIdx:
+function closestIdx = ComputeClosestIdx(x, y, traj_struct)
+% Returns the index of the closest point on the trajectory to (x, y)
 
-dims = size(traj_struct.X);
+% Extract trajectory points
+trajX = traj_struct.X(:);
+trajY = traj_struct.Y(:);
 
-% if column
-if dims(1)>dims(2)
-    % conversion of the trajectory from the struct form to a vector form
-    traj = [traj_struct.X'; traj_struct.Y'];
-else
-    % conversion of the trajectory from the struct form to a vector form
-    traj = [traj_struct.X; traj_struct.Y];
+% Compute squared distances all at once (no repmat)
+dx = trajX - x;
+dy = trajY - y;
+distsq = dx.^2 + dy.^2;
+
+% Find index of minimum distance
+[~, closestIdx] = min(distsq);
 end
 
-traj_length = max(dims);
-% computation of closest idx
-[dist,closestIdx] = min(sum((traj-repmat([x;y],1,traj_length)).^2));
-end
 
