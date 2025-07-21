46375811e5789467e020c88bcbb4a33289bddc16
Daniele Signori <daniele.signori00@gmail.com>
2025-07-21 03:36:36 +0200

    [Autonoma] minor fix

--- polimove/autonoma/opponent_comparison/func/update_gg_plot.m
+++ polimove/autonoma/opponent_comparison/func/update_gg_plot.m
@@ -5,7 +5,6 @@ function update_gg_plot()
     cla(axB); hold(axB,'on'); 
     scatter(axB, shared.best_laps{1,1}.ay_smooth, shared.best_laps{1,1}.ax_smooth, 'DisplayName','Ego','Color',shared.colors(1,:));
     for kk = get_selected_opponents(fig)'+1
-        kk
         scatter(axB, shared.best_laps{kk,1}.ay_smooth, shared.best_laps{kk,1}.ax_smooth, 'DisplayName', shared.name_map(kk-1), 'Color',shared.colors(kk+1,:));
     end
     xlabel(axB,'Lateral Acceleration (m/s²)'); ylabel(axB,'Longitudinal Acceleration (m/s²)'); legend(axB,'Location','northeast');
