4846324119061a593dc51f9087e654e537d3c7f3
Daniele Signori <daniele.signori00@gmail.com>
2025-08-07 16:48:30 +0200

    [Autonoma] Opp Comparison using telemetry msg

--- polimove/autonoma/opponent_comparison/main_telemetry.m
+++ polimove/autonoma/opponent_comparison/main_telemetry.m
@@ -7,19 +7,13 @@ clearvars -except log log_ego trajDatabase ego.index v2v.index opp_file
 %#ok<*NASGU>
 %#ok<*SAGROW>
 
-
 %% Flags
 multi_run           = false;                % if true, a different mat for ego and opponent will be loaded
 ego_vs_ego          = false;               % if true, ego vs ego will be plotted
 save_v2v            = false;               % if true, save the processed v2v data
-% opponent            = containers.Map({'TUM','TII','UNIMORE','KINETIZ'}, [1,2,3,4]); 
-% opponent            = containers.Map({'KINETIZ','TII','FR4IAV','CODE19','FLYEAGLE'},[1,2,3,4,5]); % autonomodrome - qualifying
- opponent            = containers.Map({'KINETIZ','TUM','CONSTRUCTOR','UNIMORE','TII'}, [1,2,3,4,5]); %autonomodrome - final
-
+opponent            = containers.Map({'TUM','TII','UNIMORE','TGM','CODE19','KINETIZ'}, [1,2,3,4,5,6]); 
 
 %% Paths
-
-import casadi.*;
 
 addpath("../../common/utilities/")
 addpath("../../common/constants/")
@@ -54,6 +48,8 @@ if(~exist('trajDatabase','var'))
 end
 
 % load log
+fprintf("Loading data...")
+
 if(~exist('log','var'))
     if(ego_vs_ego)
         [opp_file,path] = uigetfile(fullfile(bags,'*.mat'),'Load ego2 log');
@@ -74,24 +70,27 @@ else
     log_ego = log;
 end
 
-[v2v,ego] = get_data(log, log_ego, ego_vs_ego);
-v2v = valid_opponents(v2v);
-
+[v2v,ego] = get_data_telemetry(log, log_ego, ego_vs_ego);
+fprintf(" done. \n")
 DateTime = datetime(log.time_offset_nsec,'ConvertFrom','epochtime','TicksPerSecond',1e9,'Format','dd-MMM-yyyy HH:mm:ss');
 
 %% PROCESSING
 
 % Compute ego closest idx
 if ~exist('ego.index', 'var')
+    fprintf("Computing ego indexes ...")
     ego.index = NaN(size(ego.x));
     ego.index = compute_idx_sim(ego.x,ego.y, trajDatabase(10).X(:), trajDatabase(10).Y(:));
+    fprintf(" done. \n")
 end
 
 if ~exist('v2v.index', 'var')
+    fprintf("Computing opponent indexes ...")
     v2v.index = NaN(size(v2v.x));
     for i=1:size(v2v.x,2)
         v2v.index(:,i) = compute_idx_sim(v2v.x(:,i),v2v.y(:,i), trajDatabase(10).X(:), trajDatabase(10).Y(:));
     end
+    fprintf(" done. \n")
 end
 
 % Assign lap number
@@ -128,7 +127,6 @@ for k = 1:v2v.max_opp
     end
 end
 
-
 %% SAVE PROCESSED DATA
 
 if(save_v2v)
@@ -138,10 +136,6 @@ if(save_v2v)
     save(output_filename, 'v2v');
     fprintf('Processed data saved: %s\n', output_filename);
 end
-
-% % compute lateral acceleration and curvature
-% best_laps = fit_best_laps(v2v, log, opp_file);
-
 
 %% PLOTTING
 
@@ -160,62 +154,19 @@ setappdata(fig_traj, 'popup_ego', popup_ego);
 setappdata(fig_traj, 'popup_opp', popup_opp);
 setappdata(fig_traj, 'ax', ax_traj);
 
-% === SPEED ===
-[fig_speed, panel_speed, checkbox_speed, popup_ego_speed, popup_opp_speed] = ...
-    create_figures('Speed Profile', checklist_strings, default_selection, v2v, ego, 'speed');
-ax_speed = create_axes_layout(fig_speed, 2);
-setappdata(fig_speed, 'checklist', checkbox_speed);
-setappdata(fig_speed, 'popup_ego', popup_ego_speed);
-setappdata(fig_speed, 'popup_opp', popup_opp_speed);
-setappdata(fig_speed, 'ax', ax_speed(1));
-setappdata(fig_speed, 'ax_lapdiff', ax_speed(2));
-
-% === ACCELERATION ===
-[fig_curv, panel_curv, checkbox_curv, ~, ~] = ...
-    create_figures('Acceleration Profile', checklist_strings, default_selection, v2v, ego, 'curv');
-ax_curv = create_axes_layout(fig_curv, 3);
-setappdata(fig_curv, 'checklist', checkbox_curv);
-setappdata(fig_curv, 'ax_a', ax_curv(1));
-setappdata(fig_curv, 'ax_ax', ax_curv(2));
-setappdata(fig_curv, 'ax_ay', ax_curv(3));
-
-
 % Store shared data in base workspace or a struct accessible to both callbacks
-sharedData.ego.x = ego.x;
-sharedData.ego.y = ego.y;
-sharedData.ego.laps = ego.laps;
-sharedData.ego.index = ego.index;
-sharedData.ego.vx = ego.vx;
-sharedData.ego.laptime = ego.laptime;
-sharedData.ego.laptime_prog = ego.laptime_prog;
-sharedData.v2v.x = v2v.x;
-sharedData.v2v.y = v2v.y;
-sharedData.v2v.laps = v2v.laps;
-sharedData.v2v.index = v2v.index;
-sharedData.v2v.vx = v2v.vx;
-sharedData.v2v.laptime = v2v.laptime;
-sharedData.v2v.laptime_prog = v2v.laptime_prog;
-sharedData.v2v.max_opp = v2v.max_opp;
+sharedData.ego = ego;
+sharedData.v2v = v2v;
 sharedData.colors = colors;
 sharedData.name_map = name_map;
 sharedData.trajDatabase = trajDatabase;
 sharedData.lap_ego = 1;
 sharedData.lap_opp = 1;
-% sharedData.best_laps = best_laps;
+sharedData.ego_vs_ego = ego_vs_ego;
 setappdata(0, 'sharedData', sharedData);
 
 % Initial plots, lap 1
 update_trajectory_laps();
-update_speed_laps();
-%update_acc_fig();
 
-% Link x-axis limits manually between Speed and Curvature figures
-ax_speed = getappdata(fig_speed, 'ax');
-ax_lapdiff = getappdata(fig_speed, 'ax_lapdiff');
-% ax_a = getappdata(fig_curv, 'ax_a');
-% ax_ax = getappdata(fig_curv, 'ax_ax');
-% ax_ay = getappdata(fig_curv, 'ax_ay');
-% linkaxes([ax_speed, ax_lapdiff], 'x');
-% linkaxes([ax_a, ax_ax, ax_ay], 'x');
 
 
