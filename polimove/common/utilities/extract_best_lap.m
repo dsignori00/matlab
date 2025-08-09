d7ad9991433239ed71ac7d7ff47ede9735769d02
Daniele Signori <daniele.signori00@gmail.com>
2025-08-09 23:59:41 +0200

    [Autonoma] fixed acc and curv computation

--- polimove/common/utilities/extract_best_lap.m
+++ polimove/common/utilities/extract_best_lap.m
@@ -7,28 +7,32 @@ function best_lap = extract_best_lap(dataset, VEH_IDX, LAP_TIME_MIN, LAP_TIME_MA
     for i=1:N_laps
     
         curr_lap_time = max(dataset.laptime_prog(dataset.laps(:, VEH_IDX) == i, VEH_IDX));
-    
-        if curr_lap_time > LAP_TIME_MIN
-            lap_times(i) = max(dataset.laptime_prog(dataset.laps(:, VEH_IDX) == i, VEH_IDX));
+
+        if curr_lap_time < LAP_TIME_MIN || curr_lap_time > LAP_TIME_MAX
+            lap_times(i) = NaN;
         else
-            lap_times(i) = LAP_TIME_MAX;
+            lap_times(i) = curr_lap_time;
         end
     end
 
-    [~, best_lap_idx] = min(lap_times);
+    [min_time, best_lap_idx] = min(lap_times, [], 'omitnan');
 
-    idx_vec = dataset.laps(:, VEH_IDX) == best_lap_idx;
+    if isnan(min_time)        
+        best_lap = fill_with_nan();
+    else
+        idx_vec = dataset.laps(:, VEH_IDX) == best_lap_idx;
 
-    x = dataset.x(idx_vec, VEH_IDX);
-    y = dataset.y(idx_vec, VEH_IDX);
-    v = dataset.vx(idx_vec, VEH_IDX)./3.6;
-    yaw = dataset.yaw(idx_vec, VEH_IDX);
+        x = dataset.x(idx_vec, VEH_IDX);
+        y = dataset.y(idx_vec, VEH_IDX);
+        v = dataset.vx(idx_vec, VEH_IDX)./3.6;
+        yaw = dataset.yaw(idx_vec, VEH_IDX);
 
-    best_lap.x = x;
-    best_lap.y = y;
-    best_lap.v = v;
-    best_lap.yaw = yaw;
-    best_lap.s = [0; cumsum(sqrt(diff(x).^2 + diff(y).^2))];
+        best_lap.x   = x;
+        best_lap.y   = y;
+        best_lap.v   = v;
+        best_lap.yaw = yaw;
+        best_lap.s   = [0; cumsum(sqrt(diff(x).^2 + diff(y).^2))];
+    end
 
 end
 
