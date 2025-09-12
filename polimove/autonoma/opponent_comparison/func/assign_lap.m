bc23aa1af180e31997b45e40ec137f0e36af5553
Daniele Signori <daniele.signori00@gmail.com>
2025-09-12 17:24:01 +0200

    [Autonoma] hungarian algorithm to match track ids

--- polimove/autonoma/opponent_comparison/func/assign_lap.m
+++ polimove/autonoma/opponent_comparison/func/assign_lap.m
@@ -3,20 +3,28 @@ function laps = assign_lap(indices)
     if ~isvector(indices)
         error('Input must be a vector.');
     end
+    
+    % To avoid jumps in lap when teleporting
+    % % Calculate the 90th percentile threshold
+    % threshold = prctile(indices, 90);
+    % 
+    % % Check that the value before the drop is above the 90th percentile
+    % high_prev = [false; indices(1:end-1) > threshold];
 
-    % Calculate the 90th percentile threshold
-    threshold = prctile(indices, 90);
-
-    % Find positions where the index drops
-    diffs = diff(indices);
-    dec_indices = [false; abs(diffs) > 30];
-
-    % Check that the value before the drop is above the 90th percentile
-    %high_prev = [false; indices(1:end-1) > threshold];
-
-    % Combine both conditions with logical AND
-    drops = dec_indices; %& high_prev;
-
-    % Compute cumulative sum of lap changes
+    % Logical mask dei valori validi
+    validMask = ~isnan(indices);
+    
+    % Differenze solo sui valori validi
+    diffs = diff(indices(validMask));
+    
+    % Trova i punti dove c'è un drop
+    dec_indices = [false; diffs < -20];
+    
+    % Ricostruisci un vettore 'drops' della stessa lunghezza di indices
+    drops = false(size(indices));
+    drops(validMask) = dec_indices;
+    
+    % Cumulative sum per i giri
     laps = cumsum(drops) + 1;
+
 end
