ac36d23db1e785b2555f4720962cfd3fe77106c8
Daniele Signori <daniele.signori00@gmail.com>
2025-11-12 15:25:34 +0400

    [Tracking] checks on log struct

--- polimove/perception/utils/load_perception.m
+++ polimove/perception/utils/load_perception.m
@@ -1,91 +1,127 @@
+% Helper function to create an empty detection struct
+emptyDetectionStruct = struct( ...
+    'sens_stamp', NaN, ...
+    'stamp', NaN, ...
+    'x_rel', NaN, ...
+    'y_rel', NaN, ...
+    'z_rel', NaN, ...
+    'yaw_rel', NaN, ...
+    'x_map', NaN, ...
+    'y_map', NaN, ...
+    'z_map', NaN, ...
+    'yaw_map', NaN ...
+);
+
+% Add optional radar-specific field
+emptyRadarStruct = emptyDetectionStruct;
+emptyRadarStruct.rho_dot = NaN;
+
 % LIDAR CLUSTERING DETECTIONS
-lid_clust.sens_stamp = log.perception__lidar__clustering_detections.sensor_stamp__tot;
-lid_clust.stamp = log.perception__lidar__clustering_detections.stamp__tot;
-% relative
-lid_clust.x_rel = log.perception__lidar__clustering_detections.detections__x_rel;
-lid_clust.y_rel = log.perception__lidar__clustering_detections.detections__y_rel;
-lid_clust.z_rel = log.perception__lidar__clustering_detections.detections__z_rel;
-lid_clust.yaw_rel = log.perception__lidar__clustering_detections.detections__yaw_rel;
-lid_clust.x_rel(lid_clust.x_rel==0)=nan;
-lid_clust.y_rel(lid_clust.y_rel==0)=nan;
-lid_clust.z_rel(lid_clust.z_rel==0)=nan;
-lid_clust.yaw_rel(lid_clust.yaw_rel==0)=nan;
-% map
-lid_clust.x_map = log.perception__lidar__clustering_detections.detections__x_map;
-lid_clust.y_map = log.perception__lidar__clustering_detections.detections__y_map;
-lid_clust.z_map = log.perception__lidar__clustering_detections.detections__z_map;
-lid_clust.yaw_map = log.perception__lidar__clustering_detections.detections__yaw_map;
-lid_clust.x_map(lid_clust.x_map==0)=nan;
-lid_clust.y_map(lid_clust.y_map==0)=nan;
-lid_clust.z_map(lid_clust.z_map==0)=nan;
-lid_clust.yaw_map(lid_clust.yaw_map==0)=nan;
+if exist("log.perception__lidar__clustering_detections",'var')
+    lid_clust.sens_stamp = log.perception__lidar__clustering_detections.sensor_stamp__tot;
+    lid_clust.stamp      = log.perception__lidar__clustering_detections.stamp__tot;
+    % relative
+    lid_clust.x_rel  = log.perception__lidar__clustering_detections.detections__x_rel;
+    lid_clust.y_rel  = log.perception__lidar__clustering_detections.detections__y_rel;
+    lid_clust.z_rel  = log.perception__lidar__clustering_detections.detections__z_rel;
+    lid_clust.yaw_rel= log.perception__lidar__clustering_detections.detections__yaw_rel;
+    lid_clust.x_rel(lid_clust.x_rel==0)=nan;
+    lid_clust.y_rel(lid_clust.y_rel==0)=nan;
+    lid_clust.z_rel(lid_clust.z_rel==0)=nan;
+    lid_clust.yaw_rel(lid_clust.yaw_rel==0)=nan;
+    % map
+    lid_clust.x_map  = log.perception__lidar__clustering_detections.detections__x_map;
+    lid_clust.y_map  = log.perception__lidar__clustering_detections.detections__y_map;
+    lid_clust.z_map  = log.perception__lidar__clustering_detections.detections__z_map;
+    lid_clust.yaw_map= log.perception__lidar__clustering_detections.detections__yaw_map;
+    lid_clust.x_map(lid_clust.x_map==0)=nan;
+    lid_clust.y_map(lid_clust.y_map==0)=nan;
+    lid_clust.z_map(lid_clust.z_map==0)=nan;
+    lid_clust.yaw_map(lid_clust.yaw_map==0)=nan;
+else
+    lid_clust = emptyDetectionStruct;
+end
 
 % RADAR CLUSTERING DETECTIONS
-rad_clust.sens_stamp = log.perception__radar__clustering_detections.sensor_stamp__tot;
-rad_clust.stamp = log.perception__radar__clustering_detections.stamp__tot;
-% relative
-rad_clust.x_rel = log.perception__radar__clustering_detections.detections__x_rel;
-rad_clust.y_rel = log.perception__radar__clustering_detections.detections__y_rel;
-rad_clust.z_rel = log.perception__radar__clustering_detections.detections__z_rel;
-rad_clust.yaw_rel = log.perception__radar__clustering_detections.detections__yaw_rel;
-rad_clust.rho_dot = log.perception__radar__clustering_detections.detections__rho_dot;
-rad_clust.x_rel(rad_clust.x_rel==0)=nan;
-rad_clust.y_rel(rad_clust.y_rel==0)=nan;
-rad_clust.z_rel(rad_clust.z_rel==0)=nan;
-rad_clust.yaw_rel(rad_clust.yaw_rel==0)=nan;
-rad_clust.rho_dot(rad_clust.rho_dot==0)=nan;
-% map
-rad_clust.x_map = log.perception__radar__clustering_detections.detections__x_map;
-rad_clust.y_map = log.perception__radar__clustering_detections.detections__y_map;
-rad_clust.z_map = log.perception__radar__clustering_detections.detections__z_map;
-rad_clust.yaw_map = log.perception__radar__clustering_detections.detections__yaw_map;
-rad_clust.x_map(rad_clust.x_map==0)=nan;
-rad_clust.y_map(rad_clust.y_map==0)=nan;
-rad_clust.z_map(rad_clust.z_map==0)=nan;
-rad_clust.yaw_map(rad_clust.yaw_map==0)=nan;
+if exist("log.perception__radar__clustering_detections",'var')
+    rad_clust.sens_stamp = log.perception__radar__clustering_detections.sensor_stamp__tot;
+    rad_clust.stamp      = log.perception__radar__clustering_detections.stamp__tot;
+    % relative
+    rad_clust.x_rel  = log.perception__radar__clustering_detections.detections__x_rel;
+    rad_clust.y_rel  = log.perception__radar__clustering_detections.detections__y_rel;
+    rad_clust.z_rel  = log.perception__radar__clustering_detections.detections__z_rel;
+    rad_clust.yaw_rel= log.perception__radar__clustering_detections.detections__yaw_rel;
+    rad_clust.rho_dot= log.perception__radar__clustering_detections.detections__rho_dot;
+    rad_clust.x_rel(rad_clust.x_rel==0)=nan;
+    rad_clust.y_rel(rad_clust.y_rel==0)=nan;
+    rad_clust.z_rel(rad_clust.z_rel==0)=nan;
+    rad_clust.yaw_rel(rad_clust.yaw_rel==0)=nan;
+    rad_clust.rho_dot(rad_clust.rho_dot==0)=nan;
+    % map
+    rad_clust.x_map  = log.perception__radar__clustering_detections.detections__x_map;
+    rad_clust.y_map  = log.perception__radar__clustering_detections.detections__y_map;
+    rad_clust.z_map  = log.perception__radar__clustering_detections.detections__z_map;
+    rad_clust.yaw_map= log.perception__radar__clustering_detections.detections__yaw_map;
+    rad_clust.x_map(rad_clust.x_map==0)=nan;
+    rad_clust.y_map(rad_clust.y_map==0)=nan;
+    rad_clust.z_map(rad_clust.z_map==0)=nan;
+    rad_clust.yaw_map(rad_clust.yaw_map==0)=nan;
+else
+    rad_clust = emptyRadarStruct;
+end
 
-% CAMERA CLUSTERING DETECTIONS
-cam_yolo.sens_stamp = log.perception__camera__yolo_detections.sensor_stamp__tot;
-cam_yolo.stamp = log.perception__camera__yolo_detections.stamp__tot;
-% relative
-cam_yolo.x_rel = log.perception__camera__yolo_detections.detections__x_rel;
-cam_yolo.y_rel = log.perception__camera__yolo_detections.detections__y_rel;
-cam_yolo.z_rel = log.perception__camera__yolo_detections.detections__z_rel;
-cam_yolo.yaw_rel = log.perception__camera__yolo_detections.detections__yaw_rel;
-cam_yolo.x_rel(cam_yolo.x_rel==0)=nan;
-cam_yolo.y_rel(cam_yolo.y_rel==0)=nan;
-cam_yolo.z_rel(cam_yolo.z_rel==0)=nan;
-cam_yolo.yaw_rel(cam_yolo.yaw_rel==0)=nan;
-% map
-cam_yolo.x_map = log.perception__camera__yolo_detections.detections__x_map;
-cam_yolo.y_map = log.perception__camera__yolo_detections.detections__y_map;
-cam_yolo.z_map = log.perception__camera__yolo_detections.detections__z_map;
-cam_yolo.yaw_map = log.perception__camera__yolo_detections.detections__yaw_map;
-cam_yolo.x_map(cam_yolo.x_map==0)=nan;
-cam_yolo.y_map(cam_yolo.y_map==0)=nan;
-cam_yolo.yaw_map(cam_yolo.yaw_map==0)=nan;
+% CAMERA YOLO DETECTIONS
+if exist("log.perception__camera__yolo_detections",'var')
+    cam_yolo.sens_stamp = log.perception__camera__yolo_detections.sensor_stamp__tot;
+    cam_yolo.stamp      = log.perception__camera__yolo_detections.stamp__tot;
+    % relative
+    cam_yolo.x_rel  = log.perception__camera__yolo_detections.detections__x_rel;
+    cam_yolo.y_rel  = log.perception__camera__yolo_detections.detections__y_rel;
+    cam_yolo.z_rel  = log.perception__camera__yolo_detections.detections__z_rel;
+    cam_yolo.yaw_rel= log.perception__camera__yolo_detections.detections__yaw_rel;
+    cam_yolo.x_rel(cam_yolo.x_rel==0)=nan;
+    cam_yolo.y_rel(cam_yolo.y_rel==0)=nan;
+    cam_yolo.z_rel(cam_yolo.z_rel==0)=nan;
+    cam_yolo.yaw_rel(cam_yolo.yaw_rel==0)=nan;
+    % map
+    cam_yolo.x_map  = log.perception__camera__yolo_detections.detections__x_map;
+    cam_yolo.y_map  = log.perception__camera__yolo_detections.detections__y_map;
+    cam_yolo.z_map  = log.perception__camera__yolo_detections.detections__z_map;
+    cam_yolo.yaw_map= log.perception__camera__yolo_detections.detections__yaw_map;
+    cam_yolo.x_map(cam_yolo.x_map==0)=nan;
+    cam_yolo.y_map(cam_yolo.y_map==0)=nan;
+    cam_yolo.z_map(cam_yolo.z_map==0)=nan;
+    cam_yolo.yaw_map(cam_yolo.yaw_map==0)=nan;
+else
+    cam_yolo = emptyDetectionStruct;
+end
 
 % LIDAR POINTPILLARS DETECTIONS
-lid_pp.sens_stamp = log.perception__lidar__pointpillars_detections.sensor_stamp__tot;
-lid_pp.stamp = log.perception__lidar__pointpillars_detections.stamp__tot;
-% relative
-lid_pp.x_rel = log.perception__lidar__pointpillars_detections.detections__x_rel;
-lid_pp.y_rel = log.perception__lidar__pointpillars_detections.detections__y_rel;
-lid_pp.z_rel = log.perception__lidar__pointpillars_detections.detections__z_rel;
-lid_pp.yaw_rel = log.perception__lidar__pointpillars_detections.detections__yaw_rel;
-lid_pp.x_rel(lid_pp.x_rel==0)=nan;
-lid_pp.y_rel(lid_pp.y_rel==0)=nan;
-lid_pp.z_rel(lid_pp.z_rel==0)=nan;
-lid_pp.yaw_rel(lid_pp.yaw_rel==0)=nan;
-% map
-lid_pp.x_map = log.perception__lidar__pointpillars_detections.detections__x_map;
-lid_pp.y_map = log.perception__lidar__pointpillars_detections.detections__y_map;
-lid_pp.z_map = log.perception__lidar__pointpillars_detections.detections__z_map;
-lid_pp.yaw_map = log.perception__lidar__pointpillars_detections.detections__yaw_map;
-lid_pp.x_map(lid_pp.x_map==0)=nan;
-lid_pp.y_map(lid_pp.y_map==0)=nan;
-lid_pp.z_map(lid_pp.z_map==0)=nan;
-lid_pp.yaw_map(lid_pp.yaw_map==0)=nan;
+if exist("log.perception__lidar__pointpillars_detections",'var')
+    lid_pp.sens_stamp = log.perception__lidar__pointpillars_detections.sensor_stamp__tot;
+    lid_pp.stamp      = log.perception__lidar__pointpillars_detections.stamp__tot;
+    % relative
+    lid_pp.x_rel  = log.perception__lidar__pointpillars_detections.detections__x_rel;
+    lid_pp.y_rel  = log.perception__lidar__pointpillars_detections.detections__y_rel;
+    lid_pp.z_rel  = log.perception__lidar__pointpillars_detections.detections__z_rel;
+    lid_pp.yaw_rel= log.perception__lidar__pointpillars_detections.detections__yaw_rel;
+    lid_pp.x_rel(lid_pp.x_rel==0)=nan;
+    lid_pp.y_rel(lid_pp.y_rel==0)=nan;
+    lid_pp.z_rel(lid_pp.z_rel==0)=nan;
+    lid_pp.yaw_rel(lid_pp.yaw_rel==0)=nan;
+    % map
+    lid_pp.x_map  = log.perception__lidar__pointpillars_detections.detections__x_map;
+    lid_pp.y_map  = log.perception__lidar__pointpillars_detections.detections__y_map;
+    lid_pp.z_map  = log.perception__lidar__pointpillars_detections.detections__z_map;
+    lid_pp.yaw_map= log.perception__lidar__pointpillars_detections.detections__yaw_map;
+    lid_pp.x_map(lid_pp.x_map==0)=nan;
+    lid_pp.y_map(lid_pp.y_map==0)=nan;
+    lid_pp.z_map(lid_pp.z_map==0)=nan;
+    lid_pp.yaw_map(lid_pp.yaw_map==0)=nan;
+else
+    lid_pp = emptyDetectionStruct;
+end
+
 
 
 % GROUND TRUTH
@@ -117,4 +153,14 @@ elseif(use_ref)
     gt.z_map = log_ref.z_map;
     gt.vx = log_ref.speed;
     gt.yaw_map = log_ref.yaw_map;
+end
+
+
+if all( [
+    all(structfun(@(x) all(isnan(x)), lid_clust)), ...
+    all(structfun(@(x) all(isnan(x)), rad_clust)), ...
+    all(structfun(@(x) all(isnan(x)), cam_yolo)), ...
+    all(structfun(@(x) all(isnan(x)), lid_pp)) ] )
+    
+    error('No detections found: all detection structures are empty or contain only NaN values.');
 end
--- polimove/perception/utils/load_target_tracking.m
+++ polimove/perception/utils/load_target_tracking.m
@@ -1,25 +1,29 @@
 function tt = load_target_tracking(log)
-    tt.stamp = log.perception__opponents.stamp__tot;
-    % relative
-    tt.x_rel = log.perception__opponents.opponents__x_rel;
-    tt.y_rel = log.perception__opponents.opponents__y_rel;
-    tt.rho_dot = log.perception__opponents.opponents__rho_dot;
-    tt.yaw_rel = log.perception__opponents.opponents__psi_rel;
-    tt.x_rel(tt.x_rel==0)=nan;
-    tt.y_rel(tt.y_rel==0)=nan;
-    tt.rho_dot(tt.rho_dot==0)=nan;
-    tt.yaw_rel(tt.yaw_rel==0)=nan;
-    % map
-    tt.x_map = log.perception__opponents.opponents__x_geom;
-    tt.y_map = log.perception__opponents.opponents__y_geom;
-    tt.vx = log.perception__opponents.opponents__vx;
-    tt.ax = log.perception__opponents.opponents__ax;
-    tt.yaw_map = log.perception__opponents.opponents__psi;
-    tt.count = log.perception__opponents.count;
-    tt.max_opp = max(tt.count);
-    tt.x_map(tt.x_map==0)=nan;
-    tt.y_map(tt.y_map==0)=nan;
-    tt.vx(tt.vx==0)=nan;
-    tt.ax(tt.ax==0)=nan;
-    tt.yaw_map(tt.yaw_map==0)=nan;
+    if exist("log.perception__opponents",'var')
+        tt.stamp = log.perception__opponents.stamp__tot;
+        % relative
+        tt.x_rel = log.perception__opponents.opponents__x_rel;
+        tt.y_rel = log.perception__opponents.opponents__y_rel;
+        tt.rho_dot = log.perception__opponents.opponents__rho_dot;
+        tt.yaw_rel = log.perception__opponents.opponents__psi_rel;
+        tt.x_rel(tt.x_rel==0)=nan;
+        tt.y_rel(tt.y_rel==0)=nan;
+        tt.rho_dot(tt.rho_dot==0)=nan;
+        tt.yaw_rel(tt.yaw_rel==0)=nan;
+        % map
+        tt.x_map = log.perception__opponents.opponents__x_geom;
+        tt.y_map = log.perception__opponents.opponents__y_geom;
+        tt.vx = log.perception__opponents.opponents__vx;
+        tt.ax = log.perception__opponents.opponents__ax;
+        tt.yaw_map = log.perception__opponents.opponents__psi;
+        tt.count = log.perception__opponents.count;
+        tt.max_opp = max(tt.count);
+        tt.x_map(tt.x_map==0)=nan;
+        tt.y_map(tt.y_map==0)=nan;
+        tt.vx(tt.vx==0)=nan;
+        tt.ax(tt.ax==0)=nan;
+        tt.yaw_map(tt.yaw_map==0)=nan;
+    else
+        error('No target tracking data found in the log.');
+    end
 end
