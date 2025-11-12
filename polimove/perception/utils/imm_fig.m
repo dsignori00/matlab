dd7a8cc441d69c448998b06c6c3d5307bed8569c
Daniele Signori <daniele.signori00@gmail.com>
2025-11-12 10:47:07 +0100

    [Tracking] imm plot in tt analysis

--- polimove/perception/utils/imm_fig.m
+++ polimove/perception/utils/imm_fig.m
@@ -7,7 +7,7 @@ if(imm)
     axes(f) = nexttile([1,1]);
     f=f+1;
     hold on;
-    plot(tt.stamp,tt.vx(:,1:max_opp),'Color',col.tt,'DisplayName','tt');
+    plot(tt.stamp,tt.vx(:,1:tt.max_opp),'Color',col.tt,'DisplayName','tt');
     if(use_ref || use_sim_ref)
         plot(tt.stamp_ref,tt.vx_ref,'Color',col.ref,'DisplayName','Ground Truth');
     end
@@ -19,7 +19,7 @@ if(imm)
     axes(f) = nexttile([1,1]);
     f=f+1;
     hold on;
-    plot(tt.stamp,tt.ax(:,1:max_opp),'Color',col.tt,'DisplayName','tt');
+    plot(tt.stamp,tt.ax(:,1:tt.max_opp),'Color',col.tt,'DisplayName','tt');
     if(use_sim_ref)
         plot(tt.stamp_ref,tt.ax_ref,'Color',col.ref,'DisplayName','Ground Truth');
     end
@@ -30,10 +30,10 @@ if(imm)
     axes(f) = nexttile([1,1]);
     f=f+1;
     hold on;
-    plot(tt.stamp,log.perception__opponents.opponents__ctra_prob(:,1:max_opp),'Color',col.radar,'DisplayName','CTRA');
-    plot(tt.stamp,log.perception__opponents.opponents__ctrv_prob(:,1:max_opp),'Color',col.pointpillars,'DisplayName','CTRV');
-    plot(tt.stamp,log.perception__opponents.opponents__cm_acc_prob(:,1:max_opp),'Color',col.lidar,'DisplayName','CONST ACC');
-    plot(tt.stamp,log.perception__opponents.opponents__cm_dec_prob(:,1:max_opp),'Color',col.camera,'DisplayName','CONST DEC');
+    plot(tt.stamp,log.perception__opponents.opponents__ctra_prob(:,1:tt.max_opp),'Color',col.radar,'DisplayName','CTRA');
+    plot(tt.stamp,log.perception__opponents.opponents__ctrv_prob(:,1:tt.max_opp),'Color',col.pointpillars,'DisplayName','CTRV');
+    plot(tt.stamp,log.perception__opponents.opponents__cm_acc_prob(:,1:tt.max_opp),'Color',col.lidar,'DisplayName','CONST ACC');
+    plot(tt.stamp,log.perception__opponents.opponents__cm_dec_prob(:,1:tt.max_opp),'Color',col.camera,'DisplayName','CONST DEC');
     grid on;
     title('Model Prob')
     linkaxes(axes,'x')
