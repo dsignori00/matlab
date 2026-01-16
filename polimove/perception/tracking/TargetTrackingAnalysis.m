86c4697a5a4d4fd9be21e042ae5593b8e98a4e63
Daniele Signori <daniele.signori00@gmail.com>
2026-01-16 11:44:57 +0100

    [Perception] minor changes in plots

--- polimove/perception/tracking/TargetTrackingAnalysis.m
+++ polimove/perception/tracking/TargetTrackingAnalysis.m
@@ -245,7 +245,7 @@ plot(cam_yolo.sens_stamp,cam_yolo.range(:,1:cam_yolo.max_det),'o','MarkerFaceCol
 plot(lid_pp.sens_stamp,lid_pp.range(:,1:lid_pp.max_det),'o','MarkerFaceColor',col.pointpillars,'MarkerEdgeColor',col.pointpillars,'MarkerSize',sz,'DisplayName','Lid PP');
 plot(tt.stamp, tt.range(:,1:tt.max_opp), 'Color',col.tt,'DisplayName','tt');
 if(compare); plot(tt2.stamp, tt2.range(:,1:tt2.max_opp), 'Color',col.tt2,'DisplayName',name2); end
-if(use_ref || use_sim_ref); 
+if(use_ref || use_sim_ref)
     if(use_sim_ref); gt.rho = sqrt(gt.x_rel.^2 + gt.y_rel.^2); end
     plot(gt.stamp, gt.rho, 'Color',col.ref,'DisplayName','Ground Truth'); 
 end
@@ -261,10 +261,10 @@ grid on; ylabel('rho dot [m/s]');
 
 % count
 axes(f) = nexttile([1,1]); f=f+1; hold on;
-area(tt.stamp,log.perception__opponents.opponents__rad_clust_meas(:,1:rad_clust.max_det),'FaceColor',col.radar,'EdgeColor',col.radar,'DisplayName','Rad Clust');
-area(tt.stamp,log.perception__opponents.opponents__lid_pp_meas(:,1:lid_pp.max_det),'FaceColor',col.pointpillars,'EdgeColor',col.pointpillars,'DisplayName','Lid PP');
-area(tt.stamp,log.perception__opponents.opponents__lid_clust_meas(:,1:lid_clust.max_det),'FaceColor',col.lidar,'EdgeColor',col.lidar,'DisplayName','Lid Clust');
-area(tt.stamp,log.perception__opponents.opponents__cam_yolo_meas(:,1:cam_yolo.max_det),'FaceColor',col.camera,'EdgeColor',col.camera,'DisplayName','Camera');
+area(tt.stamp,log.perception__opponents.opponents__rad_clust_meas(:,1:tt.max_opp),'FaceColor',col.radar,'EdgeColor',col.radar,'DisplayName','Rad Clust');
+area(tt.stamp,log.perception__opponents.opponents__lid_pp_meas(:,1:tt.max_opp),'FaceColor',col.pointpillars,'EdgeColor',col.pointpillars,'DisplayName','Lid PP');
+area(tt.stamp,log.perception__opponents.opponents__lid_clust_meas(:,1:tt.max_opp),'FaceColor',col.lidar,'EdgeColor',col.lidar,'DisplayName','Lid Clust');
+area(tt.stamp,log.perception__opponents.opponents__cam_yolo_meas(:,1:tt.max_opp),'FaceColor',col.camera,'EdgeColor',col.camera,'DisplayName','Camera');
 grid on; ylabel('Count'); 
 
 %% SPEED AND ACC
