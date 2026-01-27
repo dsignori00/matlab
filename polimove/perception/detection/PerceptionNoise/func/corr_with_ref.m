0fe405f3f22da6438bfa33ed341b4e6d96c98dc4
Daniele Signori <daniele.signori00@gmail.com>
2026-01-27 16:34:09 +0100

    [Detection] select range for correlation and error summary figs

--- polimove/perception/detection/PerceptionNoise/func/corr_with_ref.m
+++ polimove/perception/detection/PerceptionNoise/func/corr_with_ref.m
@@ -1,12 +1,10 @@
-function [R_x,P_x,R_y,P_y,corr_val,stamp,xe,ye] = corr_with_ref(sensor, corr_stamp, corr_value)
+function [R_x,P_x,R_y,P_y] = corr_with_ref(sensor, idx, corr_stamp, corr_value)
     
-    valid = ~isnan(sensor.x_map_err);  
-
     stamp = repmat(sensor.sens_stamp, 1, size(sensor.x_map_err,2));
-    stamp = stamp(valid);
+    stamp = stamp(idx);
 
-    xe = sensor.x_map_err(valid);
-    ye = sensor.y_map_err(valid);
+    xe = sensor.x_map_err(idx);
+    ye = sensor.y_map_err(idx);
 
     % Interpola corr_value ai timestamp corrispondenti
     corr_val = interp1(corr_stamp, corr_value, stamp, 'linear', 'extrap');
