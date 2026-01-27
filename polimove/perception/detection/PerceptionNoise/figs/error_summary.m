c09e64241508245aa8cba82b3a8f62b82db269a7
Daniele Signori <daniele.signori00@gmail.com>
2026-01-27 12:12:19 +0100

    [detection] fixed heading boxplots

--- polimove/perception/detection/PerceptionNoise/figs/error_summary.m
+++ polimove/perception/detection/PerceptionNoise/figs/error_summary.m
@@ -14,27 +14,28 @@ end
 xline(0,'--','LineWidth',0.3,'HandleVisibility','off')
 yline(0,'--','LineWidth',0.3,'HandleVisibility','off')
 title('Positional Errors')
-xlabel('Y Rel [m]'); ylabel('X Rel [m]')
+xlabel('y rel [m]'); ylabel('x rel [m]')
 legend
 
 %% --- Range-rate error (Radar only) ---
-subplot(2,2,2); hold on; grid on
-boxplot(sensors{2}.s.rho_dot_err,'Symbol','','Orientation','horizontal')
-xline(0,'--','LineWidth',0.3,'HandleVisibility','off')
-title('Range Rate Error')
-xlabel('[m/s]')
-ylim([0.8 1.2])
-xlim([-sensors{2}.s.rho_dot_std sensors{2}.s.rho_dot_std])
+rho_dot_idx = find(cellfun(@(s) s.has_rho_dot, sensors));
+s = sensors{rho_dot_idx(1)};
+ax1 = subplot(2,2,2); hold on; grid on
+boxplot(s.s.rho_dot_err(:), 'Symbol', '')
+xline(0, '--', 'LineWidth', 0.3, 'HandleVisibility', 'off')
+title('Range Rate Error'); ylabel('range rate [m/s]'); xlabel(s.name); 
+ylim([-s.s.rho_dot_std, s.s.rho_dot_std]), xlim([0.8 1.2]); xticklabels([])
+
 
 %% --- Heading error ---
-subplot(2,2,4); hold on; grid on
+ax2 = subplot(2,2,4); hold on; grid on
 
 yaw_err = [];
 group   = {};
 YawMaxStd = 0;
 
 for i = 1:numel(sensors)
-    e = rad2deg(sensors{i}.s.yaw_map_err);
+    e = rad2deg(sensors{i}.s.yaw_map_err(:));
     yaw_err = [yaw_err; e];
     group   = [group; repmat({sensors{i}.name}, numel(e), 1)];
     YawMaxStd = max(YawMaxStd, rad2deg(sensors{i}.s.yaw_map_std));
