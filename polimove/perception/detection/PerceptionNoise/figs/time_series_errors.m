caac08ca8bbdca3b341e7916875f0c7c81a04c7f
Daniele Signori <daniele.signori00@gmail.com>
2026-01-27 14:19:57 +0100

    [Detection] fixed correlations fig

--- polimove/perception/detection/PerceptionNoise/figs/time_series_errors.m
+++ polimove/perception/detection/PerceptionNoise/figs/time_series_errors.m
@@ -14,7 +14,7 @@ for i = 1:numel(sensors)
     ax(f) = nexttile; hold on; grid on; f=f+1;
     plot(t0, zeros(size(t0)),'--k','LineWidth',0.3, 'HandleVisibility','off')
     plot_detections(sensors{i}.s.sens_stamp, sensors{i}.s.x_map_err,sensors{i}.s.max_det, sensors{i}.col, sensors{i}.name)
-    xlim([0 inf]); ylim([-10 10]), ylabel('error [m]'), legend show;
+    xlim([0 inf]); ylim(y_err_lim), ylabel('error [m]'), legend show;
 end
 xlabel('timestamp [s]');
 
@@ -26,7 +26,7 @@ for i = 1:numel(sensors)
     ax(f) = nexttile; hold on; grid on; f=f+1;
     plot(t0, zeros(size(t0)),'--k','LineWidth',0.3, 'HandleVisibility','off')
     plot_detections(sensors{i}.s.sens_stamp, sensors{i}.s.y_map_err,sensors{i}.s.max_det, sensors{i}.col, sensors{i}.name)
-    xlim([0 inf]); ylim([-10 10]), ylabel('error [m]'), legend show;
+    xlim([0 inf]); ylim(y_err_lim), ylabel('error [m]'), legend show;
 end
 xlabel('timestamp [s]');
 
@@ -38,6 +38,6 @@ for i = 1:numel(sensors)
     ax(f) = nexttile; hold on; grid on; f=f+1;
     plot(t0, zeros(size(t0)),'--k','LineWidth',0.3, 'HandleVisibility','off')
     plot_detections(sensors{i}.s.sens_stamp, rad2deg(sensors{i}.s.yaw_map_err),sensors{i}.s.max_det, sensors{i}.col, sensors{i}.name)
-    xlim([0 inf]); ylim([-10 10]), ylabel('error [deg]'), legend show;
+    xlim([0 inf]); ylim(y_err_lim), ylabel('error [deg]'), legend show;
 end
 xlabel('timestamp [s]');
--- polimove/perception/detection/PerceptionNoise/func/corr_with_ref.m
+++ polimove/perception/detection/PerceptionNoise/func/corr_with_ref.m
@@ -1,13 +1,18 @@
 function [R_x,P_x,R_y,P_y,corr_val,stamp,xe,ye] = corr_with_ref(sensor, corr_stamp, corr_value)
+    
+    valid = ~isnan(sensor.x_map_err);  
 
-    valid = ~isnan(sensor.x_map_err);
+    stamp = repmat(sensor.sens_stamp, 1, size(sensor.x_map_err,2));
+    stamp = stamp(valid);
 
-    stamp = sensor.sens_stamp(valid);
-    xe    = sensor.x_map_err(valid);
-    ye    = sensor.y_map_err(valid);
+    xe = sensor.x_map_err(valid);
+    ye = sensor.y_map_err(valid);
 
+    % Interpola corr_value ai timestamp corrispondenti
     corr_val = interp1(corr_stamp, corr_value, stamp, 'linear', 'extrap');
 
+    % Correlazione
     [R_x,P_x] = corrcoef(corr_val, abs(xe));
     [R_y,P_y] = corrcoef(corr_val, abs(ye));
+
 end
