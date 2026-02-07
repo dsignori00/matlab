1557a0df3ecf8346dfcce9c8889fdcb37f574cda
Daniele Signori <daniele.signori00@gmail.com>
2026-02-07 01:25:16 +0100

    [TargetTracking] added yaw rate as state variable

--- polimove/perception/tracking/TargetTrackingAnalysis/figs/speed_acc.m
+++ polimove/perception/tracking/TargetTrackingAnalysis/figs/speed_acc.m
@@ -1,6 +1,6 @@
 %% SPEED AND ACC
 figure('name', 'Filter - Speed Acc');
-tiledlayout(2,1,'Padding','compact');
+tiledlayout(3,1,'Padding','compact');
 
 % vx
 axes(f) = nexttile([1,1]); f=f+1; hold on;
@@ -14,4 +14,10 @@ axes(f) = nexttile([1,1]); f=f+1; hold on;
 plot_tt(tt.stamp, tt.ax, tt.max_opp, col.tt, name1);
 if(compare); plot_tt(tt2.stamp, tt2.ax, tt2.max_opp, col.tt2, name2); end
 if(use_sim_ref); plot(gt.stamp,gt.ax,'Color',col.ref,'DisplayName','gt'); end
-grid on; ylabel('ax [m/s$^2$]'); legend show; xlabel('timestamp [s]');
+grid on; ylabel('ax [m/s$^2$]'); legend show; 
+
+% yaw rate
+axes(f) = nexttile([1,1]); f=f+1; hold on;
+plot_tt(tt.stamp, (tt.yaw_rate), tt.max_opp, col.tt, name1);
+if(compare); plot_tt(tt2.stamp, (tt2.yaw_rate), tt2.max_opp, col.tt2, name2); end
+grid on; ylabel('yaw rate [deg/s]'); legend show; xlabel('timestamp [s]');
