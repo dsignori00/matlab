36fdaa763c09033a0ece3b3237cb744a359016d9
Daniele Signori <daniele.signori00@gmail.com>
2026-04-12 14:43:09 +0200

    [Tracking] minor fix

--- polimove/perception/tracking/TargetTrackingAnalysis/figs/dataset_analysis.m
+++ polimove/perception/tracking/TargetTrackingAnalysis/figs/dataset_analysis.m
@@ -19,26 +19,32 @@ function refreshTimeButtonPushed(~, ~)
     use_ref      = evalin('base','use_ref');
     use_sim_ref  = evalin('base','use_sim_ref');
 
+    gt_on = false;
     if use_ref || use_sim_ref
         gt = evalin('base','gt');
+        gt_on = true;
     end
 
     % --- time window ---
     t_lim = xlim(ax(1));
 
     [t1_log, tend_log] = timeWindowIdx(tt.stamp, t_lim);
-    if use_ref || use_sim_ref
-        [t1_gt, tend_gt] = timeWindowIdx(gt.stamp, t_lim);
-    end
+    if gt_on; [t1_gt, tend_gt] = timeWindowIdx(gt.stamp, t_lim); end
 
-    % --- range ---
-    tiledlayout(2,2);
+    if gt_on
+        tiledlayout(2,2);
+    else
+        tiledlayout(2,1);
+    end
     nexttile; cla reset; hold on; grid on;
 
-    data = gt.rho(t1_gt:tend_gt);
-    [counts, edges] = histcounts(data);
-    centers = edges(1:end-1) + diff(edges)/2;
-    plot(centers, counts, 'Color', col.ref, 'DisplayName', 'gt');
+    % --- range ---
+    if gt_on 
+        data = gt.rho(t1_gt:tend_gt);
+        [counts, edges] = histcounts(data);
+        centers = edges(1:end-1) + diff(edges)/2;
+        plot(centers, counts, 'Color', col.ref, 'DisplayName', 'gt');
+    end
 
     % --- sensors ---
     for i = 1:numel(sensors)
@@ -69,27 +75,29 @@ function refreshTimeButtonPushed(~, ~)
         centers = edges(1:end-1) + diff(edges)/2;
         plot(centers, counts, 'LineWidth', 1.5, 'Color', sensors{k}.col, 'DisplayName', names(k));
     end
-
+    xlim([0 inf]);
+    legend show
     ylabel('count');
     xlabel('detection rate [Hz]');
 
-    % --- velocity ---
-    nexttile; cla reset; hold on; grid on;
-    data = gt.vx(t1_gt:tend_gt);
-    [counts, edges] = histcounts(data);
-    centers = edges(1:end-1) + diff(edges)/2;
-    plot(centers, counts, 'DisplayName', 'gt', 'Color', col.ref);
-
-    ylabel('count');
-    xlabel('vx [m/s]');
-
-    % --- acceleration ---
-    nexttile; cla reset; hold on; grid on;
-    data = gt.ax(t1_gt:tend_gt);
-    [counts, edges] = histcounts(data);
-    centers = edges(1:end-1) + diff(edges)/2;
-    plot(centers, counts, 'DisplayName', 'gt', 'Color', col.ref);
-    ylabel('count');
-    xlabel('ax [m/s]');
-
+    if gt_on
+        % --- velocity ---
+        nexttile; cla reset; hold on; grid on;
+        data = gt.vx(t1_gt:tend_gt);
+        [counts, edges] = histcounts(data);
+        centers = edges(1:end-1) + diff(edges)/2;
+        plot(centers, counts, 'DisplayName', 'gt', 'Color', col.ref);
+    
+        ylabel('count');
+        xlabel('vx [m/s]');
+    
+        % --- acceleration ---
+        nexttile; cla reset; hold on; grid on;
+        data = gt.ax(t1_gt:tend_gt);
+        [counts, edges] = histcounts(data);
+        centers = edges(1:end-1) + diff(edges)/2;
+        plot(centers, counts, 'DisplayName', 'gt', 'Color', col.ref);
+        ylabel('count');
+        xlabel('ax [m/s]');
+    end
 end
