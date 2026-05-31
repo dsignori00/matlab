9082f3c3838f671e9210a4217da4699fb380be56
Daniele Signori <daniele.signori00@gmail.com>
2026-05-31 12:14:58 +0200

    [General] removed autonoma and offraod folders

--- polimove/autonoma/opponent_comparison/constants/PlottingStyle.m
+++
@@ -1,5 +0,0 @@
-% style
-set(0,'DefaultFigureWindowStyle','docked');
-set(0,'DefaultTextInterpreter', 'none');
-set(0,'DefaultLegendInterpreter', 'none');
-set(0, 'DefaultLineLineWidth', 2);
--- polimove/autonoma/opponent_comparison/func/figure_name.m
+++
@@ -1,8 +0,0 @@
-function nm = figure_name(tag)
-    switch tag
-        case 'traj',  nm = 'Trajectory';
-        case 'speed', nm = 'Speed Profile';
-        case 'acc',   nm = 'Acceleration Profile';
-        case 'gg',    nm = 'GG Plot';
-    end
-end
